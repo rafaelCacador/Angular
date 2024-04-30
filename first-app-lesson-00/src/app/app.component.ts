3b70877782d8864c9db8c5eeb28de96344d67cc6
Rafael Cacador <rafael.dioscanio@viannasempre.com.br>
2024-04-30 16:09:14 -0300

    Segunda lição, criando components pelo terminal

--- first-app-lesson-00/src/app/app.component.ts
+++ first-app-lesson-00/src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [],
-  template: `<h1>Hello BuddyS</h1>`,
-  styleUrls: ['./app.component.css'],
+  selector: 'app-root', //Como o ângular se refere ao component
+  standalone: true, //Se o component requer um arquivo .NgModule (API)
+  imports: [
+    HomeComponent,
+  ], //Dependências do componente
+  template: ` 
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+  
+`, //Marcação html e layout do componente
+  styleUrls: ['./app.component.css'], //URL do arquivo css
 })
 export class AppComponent {
   title = 'homez';
